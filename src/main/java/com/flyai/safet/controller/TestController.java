68f17d64aa255ad9e34e5794cebc21ee4d51b5d0
YuJin Kwon <tokyj515@naver.com>
2024-08-03 18:36:42 +0900

    feat: swagger 설정 추가

--- src/main/java/com/flyai/safet/controller/TestController.java
+++ src/main/java/com/flyai/safet/controller/TestController.java
@@ -4,7 +4,7 @@ package com.flyai.safet.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/test")
+@RestController()
 public class TestController {
 
 
