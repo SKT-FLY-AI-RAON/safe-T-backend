1bbb994b81d05d5f40a6294343807ad3693de8d3
YuJin Kwon <tokyj515@naver.com>
2024-08-03 18:24:31 +0900

    init commit

---
+++ src/test/java/com/flyai/safet/SafeTApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flyai.safet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeTApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
