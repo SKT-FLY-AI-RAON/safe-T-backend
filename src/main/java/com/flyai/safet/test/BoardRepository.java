c0f8ddd4838ddbb17ba5d5c5415a3a7581ebe8c8
YuJin Kwon <tokyj515@naver.com>
2024-08-04 17:49:38 +0900

    feat: 디비 연결 테스트2

--- src/main/java/com/flyai/safet/test/BoardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.flyai.safet.test;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BoardRepository extends JpaRepository<Board, Long> {
-}
