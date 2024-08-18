74fc9d8c0bd96b29aa01aa569dcce3246086b507
BoyoungH <hyong0903@naver.com>
2024-08-19 00:23:29 +0900

    Feat: SettingController에서 saveSetting 메소드 추가

---
+++ src/main/java/com/flyai/safet/controller/SettingController.java
@@ -0,0 +1,22 @@
+package com.flyai.safet.controller;
+
+
+import com.flyai.safet.dto.SettingDto;
+import com.flyai.safet.entity.ApiResponse;
+import com.flyai.safet.service.SettingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/setting")
+@RequiredArgsConstructor
+public class SettingController {
+    private final SettingService settingService;
+
+
+    @PostMapping("/")
+    public ApiResponse<String> saveSetting(@RequestBody SettingDto.SettingRequestDto requestDto) {
+        settingService.saveSetting(requestDto);
+        return new ApiResponse<>("설정이 성공적으로 수행되었습니다.");
+    }
+}
