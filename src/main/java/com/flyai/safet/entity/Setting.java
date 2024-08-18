4608e7668f70920a9c51700b6c41c0cec5905889
BoyoungH <hyong0903@naver.com>
2024-08-19 00:22:03 +0900

    Feat: Setting Entity 작성

---
+++ src/main/java/com/flyai/safet/entity/Setting.java
@@ -0,0 +1,32 @@
+package com.flyai.safet.entity;
+
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Table(name = "settings")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Setting {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "settings_id")
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "warning_option")
+    @ColumnDefault("video_option") // true:영상 띄우기, false: 소리로만 경고
+    private Boolean video_option;
+
+    @Builder
+    public Setting(User user, Boolean video_option) {
+        this.user = user;
+        this.video_option = video_option;
+    }
+}
