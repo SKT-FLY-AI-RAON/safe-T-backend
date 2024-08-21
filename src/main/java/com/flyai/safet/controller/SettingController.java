ae2128c5e900ba3bcceafe17e58d636a9e9cbdc5
YuJin <tokyj515@gmail.com>
2024-08-21 16:23:09 +0900

    feat: 유저 경고 방법 get

--- src/main/java/com/flyai/safet/controller/SettingController.java
+++ src/main/java/com/flyai/safet/controller/SettingController.java
@@ -3,6 +3,7 @@ package com.flyai.safet.controller;
 
 import com.flyai.safet.dto.SettingDto;
 import com.flyai.safet.entity.ApiResponse;
+import com.flyai.safet.entity.Setting;
 import com.flyai.safet.service.SettingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +14,12 @@ import org.springframework.web.bind.annotation.*;
 public class SettingController {
     private final SettingService settingService;
 
+
+    @GetMapping("")
+    public ApiResponse<Setting> getSetting(@RequestParam Long userId) {
+        Setting setting = settingService.getSetting(userId);
+        return new ApiResponse<>(setting);
+    }
 
     @PatchMapping("")
     public ApiResponse<String> saveSetting(@RequestBody SettingDto.SettingRequestDto requestDto) {
