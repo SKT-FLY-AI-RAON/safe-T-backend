b5a6e7dd52841b032d4a58da7964c05c21301925
BoyoungH <hyong0903@naver.com>
2024-08-21 17:12:25 +0900

    Feat: Setting api Request, Response Dto 추가

--- src/main/java/com/flyai/safet/dto/SettingDto.java
+++ src/main/java/com/flyai/safet/dto/SettingDto.java
@@ -16,10 +16,29 @@ public class SettingDto {
     @Getter
     @AllArgsConstructor
     @NoArgsConstructor
-//    @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
     @JsonInclude(JsonInclude.Include.NON_NULL)
     public static class SettingRequestDto {
         private Long userId;
         private int warningOption; //1, 2
+    }
+
+    @Builder
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public static class AgreementRequestDto {
+        private Long userId;
+        private boolean agreement;
+    }
+
+    @Builder
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public static class SettingResponseDto {
+        private int warningOption;
+        private boolean agreement;
     }
 }
